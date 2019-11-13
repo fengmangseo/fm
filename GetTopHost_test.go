c61b838a0b13d8f82f1e8cc8d707d7eac74bf4fa
Your Name <you@example.com>
2019-11-13 22:12:30 +0800

    add MD5

--- GetTopHost_test.go
+++ GetTopHost_test.go
@@ -8,5 +8,4 @@ import (
 func TestGetTopHost(t *testing.T) {
 	tophost, e := fm.GetTopHost("https://12423423.5346.a.com.cn/124")
 	t.Error(tophost, e)
-
 }
---
+++ phptogo/md5.go
@@ -0,0 +1,13 @@
+package phptogo
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func Md5(s string) string {
+	Md5Inst := md5.New()
+	Md5Inst.Write([]byte(s))
+	Result := Md5Inst.Sum([]byte(""))
+	return fmt.Sprintf("%x", Result)
+}
